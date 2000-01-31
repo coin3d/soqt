1fe9dec857ec80b2d06b4e95e0931ca230d472e3
Lars J. Aas <larsa@sim.no>
2000-01-31 15:07:21 +0000

    A new include-file for the lazily inclined...

---
+++ src/Inventor/Qt/SoQtIncludes.h
@@ -0,0 +1,106 @@
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
+
+//  $Id$
+
+#ifndef __SOQT_INCLUDES_H__
+#define __SOQT_INCLUDES_H__
+
+#ifndef __SOQT_H__
+#include <Inventor/Qt/SoQt.h>
+#endif // ! __SOQT_H__
+
+#ifndef __SOQT_COMPONENT_H__
+#include <Inventor/Qt/SoQtComponent.h>
+#endif // ! __SOQT_COMPONENT_H__
+
+#ifndef __SOQT_CURSORS_H__
+#include <Inventor/Qt/SoQtCursors.h>
+#endif // ! __SOQT_CURSORS_H__
+
+#ifndef __SOQT_GLWIDGET_H__
+#include <Inventor/Qt/SoQtGLWidget.h>
+#endif // ! __SOQT_GLWIDGET_H__
+
+#ifndef __SOQT_LIGHTSLIDERSET_H__
+#include <Inventor/Qt/SoQtLightSliderSet.h>
+#endif // ! __SOQT_LIGHTSLIDERSET_H__
+
+#ifndef __SOQT_MATERIALSLIDERSET_H__
+#include <Inventor/Qt/SoQtMaterialSliderSet.h>
+#endif // ! __SOQT_MATERIALSLIDERSET_H__
+
+#ifndef __SOQT_RENDERAREA_H__
+#include <Inventor/Qt/SoQtRenderArea.h>
+#endif // ! __SOQT_RENDERAREA_H__
+
+#ifndef __SOQT_SLIDERSET_H__
+#include <Inventor/Qt/SoQtSliderSet.h>
+#endif // ! __SOQT_SLIDERSET_H__
+
+#ifndef __SOQT_SLIDERSETBASE_H__
+#include <Inventor/Qt/SoQtSliderSetBase.h>
+#endif // ! __SOQT_SLIDERSETBASE_H__
+
+#ifndef __SOQT_TRANSFORMSLIDERSET_H__
+#include <Inventor/Qt/SoQtTransformSliderSet.h>
+#endif // ! __SOQT_TRANSFORMSLIDERSET_H__
+
+#ifndef __SOQT_DEVICE_H__
+#include <Inventor/Qt/devices/SoQtDevice.h>
+#endif // ! __SOQT_DEVICE_H__
+#ifndef __SOQT_INPUTFOCUS_H__
+#include <Inventor/Qt/devices/SoQtInputFocus.h>
+#endif // ! __SOQT_INPUTFOCUS_H__
+#ifndef __SOQT_KEYBOARD_H__
+#include <Inventor/Qt/devices/SoQtKeyboard.h>
+#endif // ! __SOQT_KEYBOARD_H__
+#ifndef __SOQT_MOUSE_H__
+#include <Inventor/Qt/devices/SoQtMouse.h>
+#endif // ! __SOQT_MOUSE_H__
+#ifndef __SOQT_SPACEBALL_H__
+#include <Inventor/Qt/devices/SoQtSpaceball.h>
+#endif // ! __SOQT_SPACEBALL_H__
+
+#ifndef __SOQT_VIEWER_H__
+#include <Inventor/Qt/viewers/SoQtViewer.h>
+#endif // ! __SOQT_VIEWER_H__
+#ifndef __SOQT_FULLVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtFullViewer.h>
+#endif // ! __SOQT_FULLVIEWER_H__
+#ifndef __SOQT_EXAMINERVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
+#endif // ! __SOQT_EXAMINERVIEWER_H__
+#ifndef __SOQT_PLANEVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtPlaneViewer.h>
+#endif // ! __SOQT_PLANEVIEWER_H__
+#ifndef __SOQT_CONSTRAINEDVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtConstrainedViewer.h>
+#endif // ! __SOQT_CONSTRAINEDVIEWER_H__
+#ifndef __SOQT_WALKVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtWalkViewer.h>
+#endif // ! __SOQT_WALKVIEWER_H__
+#ifndef __SOQT_FLYVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtFlyViewer.h>
+#endif // ! __SOQT_FLYVIEWER_H__
+#ifndef __SOQT_COLLISIONVIEWER_H__
+#include <Inventor/Qt/viewers/SoQtCollisionViewer.h>
+#endif // ! __SOQT_COLLISIONVIEWER_H__
+
+#endif // ! __SOQT_INCLUDES_H__
