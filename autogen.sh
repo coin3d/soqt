ac9e89ddcfb3e7225ff6030491ac73215b8cbc85
Lars J. Aas <larsa@sim.no>
2000-06-13 21:38:42 +0000

    General autogen+configure+build process update.

--- autogen.sh
+++ autogen.sh
@@ -7,10 +7,17 @@
 # Author: Morten Eriksen, <mortene@sim.no>. Loosely based on Ralph
 # Levien's script for Gnome.
 
-DIE=0
+directory=`echo "$0" | sed -e 's/[^\/]*$//g'`;
+cd $directory
+if ! test -f ./autogen.sh; then
+  echo "unexpected problem with your shell - bailing out"
+  exit 1
+fi
 
-PROJECT=SoQt
+GUI=Qt
+PROJECT=So$GUI
 MACRODIR=conf-macros
+DIE=false
 
 SUBPROJECTS="$MACRODIR src/Inventor/Qt/common examples/components"
 SUBPROJECTNAMES="$MACRODIR SoQtCommon SoQtComponents"
@@ -29,11 +36,12 @@ if test "$1" = "--clean"; then
         missing \
         mkinstalldirs \
         stamp-h*
-  exit
+  find . -name Makefile.in -print | \
+        egrep -v '^\./(examples|ivexamples)/' | xargs rm
+  exit 0
 elif test "$1" = "--add"; then
   AUTOMAKE_ADD="--add-missing --gnu --copy"
 fi
-
 
 echo "Checking the installed configuration tools..."
 
@@ -49,7 +57,7 @@ if test -z "`autoconf --version | grep \" $AUTOCONF_VER\" 2> /dev/null`"; then
     echo ""
     echo "   ftp://ftp.sim.no/pub/coin/autoconf-2.14.1-coin.tar.gz"
     echo ""
-    DIE=1
+    DIE=true
 fi
 
 AUTOMAKE_VER=1.4a-SIM-20000531  # Automake from CVS
@@ -63,7 +71,7 @@ if test -z "`automake --version | grep \" $AUTOMAKE_VER\" 2> /dev/null`"; then
     echo ""
     echo "   ftp://ftp.sim.no/pub/coin/automake-1.4a-coin.tar.gz"
     echo ""
-    DIE=1
+    DIE=true
 fi
 
 LIBTOOL_VER=1.3.5
@@ -74,7 +82,7 @@ if test -z "`libtool --version | egrep \"$LIBTOOL_VER\" 2> /dev/null`"; then
     echo ""
     echo "Get ftp://ftp.gnu.org/pub/gnu/libtool/libtool-1.3.5.tar.gz"
     echo ""
-    DIE=1
+    DIE=true
 fi
 
 set $SUBPROJECTNAMES
@@ -91,16 +99,13 @@ for project in $SUBPROJECTS; do
     echo "(if all else fails), remove $PROJECT and run:"
     echo "  cvs -z3 -d :pserver:cvs@cvs.sim.no:/export/cvsroot co -P $PROJECT"
     echo ""
-    DIE=1
+    DIE=true
   }
   num=`expr $num + 1`
   shift
 done
 
-if test "$DIE" -eq 1; then
-        exit 1
-fi
-
+$DIE && exit 1
 
 echo "Running aclocal (generating aclocal.m4)..."
 aclocal -I $MACRODIR
