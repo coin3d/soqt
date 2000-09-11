2c355811d2722ccee14185c850f752e3ca48a72d
Lars J. Aas <larsa@sim.no>
2000-09-11 14:59:25 +0000

    Change to LGPL header.

--- src/Inventor/Qt/editors/SoQtColorSlider.h
+++ src/Inventor/Qt/editors/SoQtColorSlider.h
@@ -1,26 +1,26 @@
-/**************************************************************************\
+/**************************************************************************
  *
- *  Copyright (C) 1998-1999 by Systems in Motion.  All rights reserved.
+ *  This file is part of the Coin SoQt GUI binding library.
+ *  Copyright (C) 1998-2000 by Systems in Motion.  All rights reserved.
  *
- *  This file is part of the Coin library.
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version
+ *  2.1 as published by the Free Software Foundation.  See the file
+ *  LICENSE.LGPL at the root directory of the distribution for all the
+ *  details.
  *
- *  This file may be distributed under the terms of the Q Public License
- *  as defined by Troll Tech AS of Norway and appearing in the file
- *  LICENSE.QPL included in the packaging of this file.
+ *  If you want to use Coin SoQt for applications not compatible with the
+ *  LGPL, please contact SIM to acquire a Professional Edition License.
  *
- *  If you want to use Coin in applications not covered by licenses
- *  compatible with the QPL, you can contact SIM to aquire a
- *  Professional Edition license for Coin.
+ *  Systems in Motion, Prof Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ support@sim.no Voice: +47 22114160 Fax: +47 22207097
  *
- *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
- *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
- *
-\**************************************************************************/
+ **************************************************************************/
 
 //  $Id$
 
-#ifndef __SOQTCOLORSLIDER_H__
-#define __SOQTCOLORSLIDER_H__
+#ifndef SOQT_COLORSLIDER_H
+#define SOQT_COLORSLIDER_H
 
 // FIXME: not in use yet. 19990620 mortene.
 
@@ -67,4 +67,4 @@ public:
 
 };
 
-#endif // !__SOQTCOLORSLIDER_H__
+#endif // ! SOQT_COLORSLIDER_H
--- src/Inventor/Qt/editors/SoQtMaterialEditor.h
+++ src/Inventor/Qt/editors/SoQtMaterialEditor.h
@@ -1,26 +1,26 @@
-/**************************************************************************\
+/**************************************************************************
  *
- *  Copyright (C) 1998-1999 by Systems in Motion.  All rights reserved.
+ *  This file is part of the Coin SoQt GUI binding library.
+ *  Copyright (C) 1998-2000 by Systems in Motion.  All rights reserved.
  *
- *  This file is part of the Coin library.
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version
+ *  2.1 as published by the Free Software Foundation.  See the file
+ *  LICENSE.LGPL at the root directory of the distribution for all the
+ *  details.
  *
- *  This file may be distributed under the terms of the Q Public License
- *  as defined by Troll Tech AS of Norway and appearing in the file
- *  LICENSE.QPL included in the packaging of this file.
+ *  If you want to use Coin SoQt for applications not compatible with the
+ *  LGPL, please contact SIM to acquire a Professional Edition License.
  *
- *  If you want to use Coin in applications not covered by licenses
- *  compatible with the QPL, you can contact SIM to aquire a
- *  Professional Edition license for Coin.
+ *  Systems in Motion, Prof Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ support@sim.no Voice: +47 22114160 Fax: +47 22207097
  *
- *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
- *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
- *
-\**************************************************************************/
+ **************************************************************************/
 
 //  $Id$
 
-#ifndef _SO_QT_MATERIAL_EDITOR_
-#define _SO_QT_MATERIAL_EDITOR_
+#ifndef SOQT_MATERIALEDITOR_H
+#define SOQT_MATERIALEDITOR_H
 
 #include <Inventor/Qt/SoQtComponent.h>
 #include <qwidget.h>
@@ -119,4 +119,4 @@ private:
         void            connectUpdaters(bool connect = true);
 };
 
-#endif
+#endif // ! SOQT_MATERIALEDITOR_H
