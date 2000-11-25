6e7c51ef1ed93634b141b3cba3335bd21f6492be
Morten Eriksen <mortene@sim.no>
2000-11-25 13:48:10 +0000

    Testcode for embedding an SoQtRenderArea within a QScrollView Qt widget.

---
+++ test-code/components/scrollview.cpp
@@ -0,0 +1,86 @@
+/*
+  Demonstrates embedding of an SoQtRenderArea within a QScrollView.
+*/
+
+/***********************************************************************/
+
+#include <Inventor/Qt/SoQt.h>
+#include <Inventor/Qt/SoQtRenderArea.h>
+#include <Inventor/nodes/SoCone.h>
+#include <Inventor/nodes/SoDirectionalLight.h>
+#include <Inventor/nodes/SoPerspectiveCamera.h>
+#include <Inventor/nodes/SoSeparator.h>
+#include <qscrollview.h>
+#include <qpushbutton.h>
+
+
+class MyScrollView : public QScrollView {
+public:
+  MyScrollView(QWidget * parent) : QScrollView(parent)
+    {
+      this->resizeContents(1000, 1000);
+      this->enableClipper(TRUE);
+
+      // Container widget for the SoQtRenderArea.
+
+      QWidget * container = new QWidget(this);
+      container->resize(200, 200);
+
+      // Construct a simple scenegraph.
+
+      SoSeparator * root = new SoSeparator;
+
+      SoDirectionalLight * light = new SoDirectionalLight;
+      light->direction.setValue(-0.5, -0.5, -0.8);
+      root->addChild(light);
+
+      SoPerspectiveCamera * camera = new SoPerspectiveCamera;
+      camera->orientation = SbRotation(SbVec3f(0, 0, 1), 0);
+      root->addChild(camera);
+  
+      SoCone * cone = new SoCone;
+      root->addChild(cone);
+  
+      // Add the renderarea.
+
+      renderarea = new SoQtRenderArea(container);
+      renderarea->setSceneGraph(root);
+      camera->viewAll(root, renderarea->getViewportRegion());
+
+      this->addChild(container, 200, 200);
+      this->showChild(container);
+    }
+
+protected:
+  void drawContents(QPainter * p, int cx, int cy, int cw, int ch)
+    {
+      p->fillRect(cx, cy, cw, ch, QColor(40, 80, 0));
+      renderarea->scheduleRedraw();
+    }
+
+private:
+  SoQtRenderArea * renderarea;
+};
+
+/***********************************************************************/
+
+int
+main(int argc, char ** argv)
+{
+  // Initialize Qt and SoQt.
+  QApplication app(argc, argv);
+  SoQt::init((QWidget *)NULL);
+
+  // Set up scrollview window.
+  MyScrollView * vp = new MyScrollView(NULL);
+  vp->viewport()->setBackgroundMode(QWidget::NoBackground);
+
+  // Map window.
+  vp->show();
+  // Set termination condition.
+  QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
+  // Start event loop.
+  SoQt::mainLoop();
+
+  return 0;
+}
