cf2d2bf3f0ac6dfd28106787c530a4e98b5ab94d
Morten Eriksen <mortene@sim.no>
2001-11-23 09:55:48 +0000

    No need to explicitly set a unique GL context id under Coin any more.
    Adds copyright header.

--- test-code/components/tripleview.cpp
+++ test-code/components/tripleview.cpp
@@ -1,3 +1,24 @@
+/**************************************************************************\
+ *
+ *  This file is part of a set of demo programs for the Coin library.
+ *  Copyright (C) 2000-2001 by Systems in Motion. All rights reserved.
+ *
+ *  This sourcecode can be redistributed and/or modified under the
+ *  terms of the GNU General Public License version 2 as published by
+ *  the Free Software Foundation. See the file COPYING at the root
+ *  directory of the distribution for more details.
+ *
+ *  As a special exception, all sourcecode of the demo examples can be
+ *  used for any purpose for licensees of the Coin Professional
+ *  Edition License, without the restrictions of the GNU GPL. See our
+ *  web pages for information about how to acquire a Professional Edition
+ *  License.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  http://www.sim.no support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *
+\**************************************************************************/
+
 /*
   This is just a simple test application to check that we can have
   SoQtComponent derived objects within other widgets.
@@ -67,13 +88,13 @@ add_view(QWidget * viewparent, SoGroup * common, SbRotation cameraorientation)
   SoQtRenderArea * area = new SoQtRenderArea(viewparent);
   area->setSceneGraph(root);
   
-  // IMPORTANT:
-  // make sure each GL context has a unique cache context id.  this is
-  // needed for TGS/SGI Inventor, and also for Coin when your scene
-  // graph contains textures, or when you've explicitely enabled
-  // render caching.
+#ifndef __COIN_
+  // IMPORTANT: make sure each GL context has a unique cache context
+  // id.  this is needed for TGS/SGI Inventor. Coin handles this
+  // automatically.
   static uint32_t contextcnt = 0;
   area->getGLRenderAction()->setCacheContext(contextcnt++);
+#endif // !__COIN__
 
   camera->viewAll(root, area->getViewportRegion());
 }
