ef2c6171b4b4cf465f66d68fc69ae16dd4fc2af3
Morten Eriksen <mortene@sim.no>
2001-12-07 14:19:52 +0000

    Turn off the generation of a backup file, as that makes the distcheck fail.

--- cfg/doxy4win.pl
+++ cfg/doxy4win.pl
@@ -1,4 +1,8 @@
-#! /usr/bin/perl -p -i.bak
+#! /usr/bin/perl -p -i
+
+# Note: ``perl'' is invoked with the ``-i'' option above (make
+# backup file when editing inline), but without a backup extension,
+# as the extra backup file screws up for ``make distcheck''.
 
 sub cygpath {
   $path=`CYGWIN= cygpath -w "$_[0]"`;
