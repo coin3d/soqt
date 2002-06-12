10a2003f7438bba52cb7dc7d3bc86a4b4bd25126
Morten Eriksen <mortene@sim.no>
2002-06-12 12:37:56 +0000

    Skeleton code for materialeditor component.

---
+++ src/Inventor/Qt/editors/SoQtMaterialEditor.cpp
@@ -0,0 +1,110 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin GUI toolkit libraries.
+ *  Copyright (C) 1998-2002 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation.  See the
+ *  file LICENSE.LGPL at the root directory of this source distribution
+ *  for more details.
+ *
+ *  If you want to use this library with software that is incompatible
+ *  licensewise with the LGPL, and / or you would like to take
+ *  advantage of the additional benefits with regard to our support
+ *  services, please contact Systems in Motion about acquiring a Coin
+ *  Professional Edition License.  See <URL:http://www.coin3d.org> for
+ *  more information.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  <URL:http://www.sim.no>, <mailto:support@sim.no>
+ *
+\**************************************************************************/
+
+#include <SoQtMaterialEditor.h>
+
+SoQtMaterialEditor::SoQtMaterialEditor(QWidget * parent,
+                                       const char * name,
+                                       SbBool embedinparent)
+{
+}
+
+SoQtMaterialEditor::SoQtMaterialEditor(QWidget * parent,
+                                       const char * name,
+                                       SbBool embedinparent,
+                                       SbBool buildnow)
+{
+}
+
+SoQtMaterialEditor::~SoQtMaterialEditor()
+{
+}
+
+void
+SoQtMaterialEditor::attach(SoMaterial * node, int index)
+{
+}
+
+void
+SoQtMaterialEditor::detach(void)
+{
+}
+
+SbBool
+SoQtMaterialEditor::isAttached(void)
+{
+}
+
+
+void
+SoQtMaterialEditor::addMaterialChangedCallback(SoQtMaterialEditorCB * f, void * userdata = NULL)
+{
+}
+
+void
+SoQtMaterialEditor::removeMaterialChangedCallback(SoQtMaterialEditorCB * f, void * userdata = NULL)
+{
+}
+
+
+void
+SoQtMaterialEditor::setUpdateFrequency(SoQtMaterialEditor::UpdateFrequency freq)
+{
+}
+
+SoQtMaterialEditor::UpdateFrequency
+SoQtMaterialEditor::getUpdateFrequency(void)
+{
+}
+
+void
+SoQtMaterialEditor::setMaterial(const SoMaterial & node)
+{
+}
+
+const SoMaterial &
+SoQtMaterialEditor::getMaterial(void) const
+{
+}
+
+
+QWidget *
+SoQtMaterialEditor::buildWidget(QWidget * parent)
+{
+}
+
+
+const char *
+SoQtMaterialEditor::getDefaultWidgetName(void) const
+{
+}
+
+const char *
+SoQtMaterialEditor::getDefaultTitle(void) const
+{
+}
+
+const char *
+SoQtMaterialEditor::getDefaultIconTitle(void) const
+{
+}
---
+++ src/Inventor/Qt/editors/SoQtMaterialEditor.h
@@ -0,0 +1,72 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin GUI toolkit libraries.
+ *  Copyright (C) 1998-2002 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation.  See the
+ *  file LICENSE.LGPL at the root directory of this source distribution
+ *  for more details.
+ *
+ *  If you want to use this library with software that is incompatible
+ *  licensewise with the LGPL, and / or you would like to take
+ *  advantage of the additional benefits with regard to our support
+ *  services, please contact Systems in Motion about acquiring a Coin
+ *  Professional Edition License.  See <URL:http://www.coin3d.org> for
+ *  more information.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  <URL:http://www.sim.no>, <mailto:support@sim.no>
+ *
+\**************************************************************************/
+
+#ifndef SOQTMATERIALEDITOR_H
+#define SOQTMATERIALEDITOR_H
+
+#include <Inventor/Qt/SoQtComponent.h>
+
+class SoMaterial;
+
+typedef void SoQtMaterialEditorCB(void * userdata, const SoMaterial * node);
+
+class SoQtMaterialEditor : public SoQtComponent {
+public:
+  enum UpdateFrequency { CONTINUOUS, AFTER_ACCEPT };
+
+  SoQtMaterialEditor(QWidget * parent = NULL,
+                     const char * name = NULL,
+                     SbBool embedinparent = TRUE);
+  ~SoQtMaterialEditor();
+
+  void attach(SoMaterial * node, int index = 0);
+  void detach(void);
+  SbBool isAttached(void);
+
+  void addMaterialChangedCallback(SoQtMaterialEditorCB * f, void * userdata = NULL);
+  void removeMaterialChangedCallback(SoQtMaterialEditorCB * f, void * userdata = NULL);
+
+  void setUpdateFrequency(SoQtMaterialEditor::UpdateFrequency freq);
+  SoQtMaterialEditor::UpdateFrequency getUpdateFrequency(void);
+
+  void setMaterial(const SoMaterial & node);
+  const SoMaterial & getMaterial(void) const;
+
+protected:
+  SoQtMaterialEditor(QWidget * parent,
+                     const char * name,
+                     SbBool embedinparent,
+                     SbBool buildnow);
+
+  QWidget * buildWidget(QWidget * parent);
+
+  virtual const char * getDefaultWidgetName(void) const;
+  virtual const char * getDefaultTitle(void) const;
+  virtual const char * getDefaultIconTitle(void) const;
+
+private:
+  class SoQtMaterialEditorP * pimpl;
+  friend class SoQtMaterialEditorP;
+};
+
+#endif // SOQTMATERIALEDITOR_H
