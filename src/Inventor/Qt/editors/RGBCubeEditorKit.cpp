968ad23102dd4a51f4ac3a94a01ab42df2594f21
Morten Eriksen <mortene@sim.no>
2002-06-12 10:32:21 +0000

    Additional FIXMEs.

--- src/Inventor/Qt/editors/RGBCubeEditorKit.cpp
+++ src/Inventor/Qt/editors/RGBCubeEditorKit.cpp
@@ -7,12 +7,14 @@ RGBCubeEditorKit::RGBCubeEditorKit(void)
 {
   SO_KIT_CONSTRUCTOR(RGBCubeEditorKit);
 
-  // FIXME: add all geometry nodes, draggers etc to the nodekit's
-  // catalog.
+  // FIXME: add all geometry nodes, draggers etc used for the RGB cube
+  // interaction to the nodekit's catalog.
 
   // SO_KIT_ADD_CATALOG_ENTRY(...);
 
   SO_KIT_ADD_FIELD(rgb, (SbColor(0.8, 0.8, 0.8)));
+  // FIXME: wire up the SoSFColor rgb field so it's automatically
+  // updated on changes.
 
   SO_KIT_INIT_INSTANCE();
 }
@@ -25,4 +27,10 @@ void
 RGBCubeEditorKit::initClass(void)
 {
   SO_KIT_INIT_CLASS(RGBCubeEditorKit, SoBaseKit, "BaseKit");
+}
+
+void
+RGBCubeEditorKit::handleEvent(SoHandleEventAction * action)
+{
+  // FIXME: catch mousepicks on the cube to "quick-set" a color.
 }
