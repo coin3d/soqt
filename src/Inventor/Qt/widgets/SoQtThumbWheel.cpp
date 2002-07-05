dbccacc7853fa3800c2b707f04e9d1cf33dccb6e
Morten Eriksen <mortene@sim.no>
2002-07-05 14:01:39 +0000

    LGPL -> GPL.

--- src/Inventor/Qt/widgets/SoQtThumbWheel.cpp
+++ src/Inventor/Qt/widgets/SoQtThumbWheel.cpp
@@ -1,20 +1,20 @@
 /**************************************************************************\
  *
- *  This file is part of the Coin GUI toolkit libraries.
+ *  This file is part of the Coin family of 3D visualization libraries.
  *  Copyright (C) 1998-2002 by Systems in Motion.  All rights reserved.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  version 2.1 as published by the Free Software Foundation.  See the
- *  file LICENSE.LGPL at the root directory of this source distribution
+ *  This library is free software; you can redistribute it and / or
+ *  modify it under the terms of the GNU General Public License
+ *  version 2 as published by the Free Software Foundation.  See the
+ *  file LICENSE.GPL at the root directory of this source distribution
  *  for more details.
  *
- *  If you want to use this library with software that is incompatible
- *  licensewise with the LGPL, and / or you would like to take
- *  advantage of the additional benefits with regard to our support
- *  services, please contact Systems in Motion about acquiring a Coin
- *  Professional Edition License.  See <URL:http://www.coin3d.org> for
- *  more information.
+ *  If you desire to use this library in software that is incompatible
+ *  with the GNU GPL, and / or you would like to take advantage of the
+ *  additional benefits with regard to our support services, please
+ *  contact Systems in Motion about acquiring a Coin Professional
+ *  Edition License.  See <URL:http://www.coin3d.org> for more
+ *  information.
  *
  *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
  *  <URL:http://www.sim.no>, <mailto:support@sim.no>
