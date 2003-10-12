1922d84f3d431c2474ee1b3666f1fdb657ced07b
Morten Eriksen <mortene@sim.no>
2003-10-12 10:39:25 +0000

    Compilation fixes: add in missing variables.

--- src/Inventor/Qt/widgets/gradientp/SoQtGradientDialogP.h
+++ src/Inventor/Qt/widgets/gradientp/SoQtGradientDialogP.h
@@ -1,6 +1,29 @@
 #ifndef SOQTGRADIENTDIALOGP_H
 #define SOQTGRADIENTDIALOGP_H
 
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
 #include <qvaluelist.h>
 #include <qobject.h>
 #include "../Gradient.h"
@@ -17,6 +40,11 @@ class SoQtGradientDialogP : public QObject {
 public:
   SoQtGradientDialogP(SoQtGradientDialog * publ);
   SoQtGradientDialog * pub;
+
+  static void gradientCallBack(const Gradient & thisp, void * userData);
+  void saveCurrent();
+
+  QFileDialog * fileDialog;
   GradientView * gradView;
   QValueList<Gradient> gradients;
   QComboBox * gradientList;
@@ -26,9 +54,13 @@ public:
   void * changeCallBackData;
   int old_index;
   void * callBackData;
+  // FIXME: static? This looks fishy, as it's a per-instance
+  // setting. 20031012 mortene.
   static SbBool updateContinuously;
-  static void gradientCallBack(const Gradient & thisp, void * userData);
-  void saveCurrent();
+
+  QLabel * contupdateLabel;
+  QPushButton * applyButton;
+  
 
 public slots:
   void loadGradient(void);
@@ -39,4 +71,4 @@ public slots:
   void resetGradient(void);
 };
 
-#endif // SOQTGRADIENTDIALOGP_H
+#endif // ! SOQTGRADIENTDIALOGP_H
