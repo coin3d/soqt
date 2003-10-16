680f244830b7912b5f63ea12cf3b1d7e97a5b0d6
Frode ?ijord <frodo@sim.no>
2003-10-16 10:35:58 +0000

    added copyright headers

--- src/Inventor/Qt/widgets/SoQtCurveWidget.h
+++ src/Inventor/Qt/widgets/SoQtCurveWidget.h
@@ -1,5 +1,28 @@
-#ifndef CURVEDIALOG_H
-#define CURVEDIALOG_H
+#ifndef SOQTCURVEWIDGET_H
+#define SOQTCURVEWIDGET_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
 
 #include <QWidget.h>
 #include <Inventor/system/inttypes.h>
@@ -27,4 +50,4 @@ private:
   class SoQtCurveWidgetP * pimpl;
 };
 
-#endif // CURVEDIALOG_H
+#endif // SOQTCURVEWIDGET_H
