7fb3ec4d181449e7c9a08a234cbd632b73847a39
Peder Blekken <pederb@sim.no>
2004-03-15 13:19:03 +0000

    gcc 3.3 compile fix.

--- src/Inventor/Qt/SoQt.cpp
+++ src/Inventor/Qt/SoQt.cpp
@@ -650,7 +650,7 @@ SoQt::init(QWidget * toplevelwidget)
     // subclass to catch spaceball events.
 
     // use a static char array to store the dummy argv parameters
-    static const char * dummyargv[1];
+    static char * dummyargv[1];
     dummyargv[0] = "SoQt";
     SoQtP::appobject = new SoQtApplication(1, dummyargv);
   }
